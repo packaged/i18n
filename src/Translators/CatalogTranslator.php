025e94c34f02e32cb41cf9dc1c17f7bf14aca1e8
Brooke Bryan <brooke@bajb.net>
2020-01-28 15:03:53 +0000

    Catalog Translator

---
+++ src/Translators/CatalogTranslator.php
@@ -0,0 +1,28 @@
+<?php
+namespace Packaged\I18n\Translators;
+
+use Packaged\I18n\Catalog\Message;
+use Packaged\I18n\Catalog\MessageCatalog;
+
+class CatalogTranslator extends AbstractTranslator
+{
+  /**
+   * @var MessageCatalog
+   */
+  protected $_catalog;
+
+  public function __construct(MessageCatalog $catalog)
+  {
+    $this->_catalog = $catalog;
+  }
+
+  public function _($msgId, $default, array $replacements = null, $choice = null): string
+  {
+    $msg = $this->_catalog->getMessage($msgId);
+    if($msg === null)
+    {
+      $msg = Message::create(null, $default);
+    }
+    return $this->_applyReplacements($msg->getText($choice), $replacements);
+  }
+}
