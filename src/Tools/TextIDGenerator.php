7d38cb934c0205cf622cae1e6158c43cd8e0f972
Brooke Bryan <brooke@bajb.net>
2020-01-28 10:08:37 +0000

    Text ID Generator

---
+++ src/Tools/TextIDGenerator.php
@@ -0,0 +1,25 @@
+<?php
+namespace Packaged\I18n\Tools;
+
+class TextIDGenerator
+{
+  protected $_minWord = 3;
+  protected $_prefixLength = 30;
+  protected $_appendBaseTime = true;
+
+  public function generateId($text)
+  {
+    if(strlen($text) > $this->_prefixLength)
+    {
+      $text1 = preg_replace('/\b\w{0,' . $this->_minWord . '}\b|\s/', ' ', $text);
+    }
+    else
+    {
+      $text1 = $text;
+    }
+
+    $shortText = preg_replace('/[^\w]+/', '_', $text1);
+    return strtolower(trim(substr($shortText, 0, $this->_prefixLength), '_')) .
+      ($this->_appendBaseTime ? '_' . base_convert(time(), 10, 36) : '');
+  }
+}
