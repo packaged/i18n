13f25ef8a6aba2284d96d9f941a91d6743f64dda
Tsaunders99 <saundersthomas43@gmail.com>
2022-06-23 18:13:12 +0100

    - Updated test script to check for translations passed

--- tests/Tools/Gettext/PoFileTest.php
+++ tests/Tools/Gettext/PoFileTest.php
@@ -10,41 +10,45 @@ class PoFileTest extends TestCase
   public function testCorrectLineSpacing()
   {
     $source = <<<TRANS
-#. Subject
-#: subject_c789
-#, fuzzy
-msgid "Subject"
-msgstr ""
+#. Translation 1
+#: translation_1_7c7c
+#, fuzzy1
+msgid "Translation 1"
+msgstr "Translation 1"
 
-#. Subject2
-#: subject_c7892
+#. Translation 2
+#: translation_2_1410
 #, fuzzy2
-msgid "Subject2"
-msgstr "Objet2"
+msgid "Translation 2"
+msgstr "Translation 2"
 TRANS;
 
     $translation = PoFile::fromString($source);
 
     self::assertCount(2, $translation->getTranslations());
+    self::assertEquals('Translation 1', $translation->getTranslations()['translation_1_7c7c']->getSingularTranslation());
+    self::assertEquals('Translation 2', $translation->getTranslations()['translation_2_1410']->getSingularTranslation());
   }
 
   public function testIncorrectLineSpacing()
   {
     $source = <<<TRANS
-#. Subject
-#: subject_c789
-#, fuzzy
-msgid "Subject"
-msgstr "Objet"
-#. Subject2
-#: subject_c7892
+#. Translation 1
+#: translation_1_7c7c
+#, fuzzy1
+msgid "Translation 1"
+msgstr "Translation 1"
+#. Translation 2
+#: translation_2_1410
 #, fuzzy2
-msgid "Subject2"
-msgstr "Objet2"
+msgid "Translation 2"
+msgstr "Translation 2"
 TRANS;
 
     $translation = PoFile::fromString($source);
 
     self::assertCount(2, $translation->getTranslations());
+    self::assertEquals('Translation 1', $translation->getTranslations()['translation_1_7c7c']->getSingularTranslation());
+    self::assertEquals('Translation 2', $translation->getTranslations()['translation_2_1410']->getSingularTranslation());
   }
 }
