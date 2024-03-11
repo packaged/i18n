8d467ded029bc1b9c707822d9892ce359d34cdad
Brooke Bryan <brooke@bajb.net>
2024-03-11 22:46:29 +0000

    Quick passthroughs

--- src/TranslatorAwareTrait.php
+++ src/TranslatorAwareTrait.php
@@ -23,4 +23,21 @@ trait TranslatorAwareTrait
   {
     return $this->_translator !== null;
   }
+
+  /** Pass through translator methods */
+
+  public function _($msgId, $default, array $replacements = null, $choice = null): string
+  {
+    return $this->getTranslator()->_($msgId, $default, $replacements, $choice);
+  }
+
+  public function _p(string $msgId, string $singular, string $plural, int $n, array $replacements = null): string
+  {
+    return $this->getTranslator()->_p($msgId, $singular, $plural, $n, $replacements);
+  }
+
+  public function _sp(string $msgId, string $simplural, int $n, array $replacements = null): string
+  {
+    return $this->getTranslator()->_sp($msgId, $simplural, $n, $replacements);
+  }
 }
